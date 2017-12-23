f89a1f21e10d238176053585263656294a6ab084
Leonardo Nunes Carvalho <worldrulesdev@gmail.com>
2017-12-23 16:44:29 -0200

    Eu quero subir essa merda
    DE UMA VEZ

---
+++ principal - Cópia.js
@@ -0,0 +1,30 @@
+﻿var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if (altura <= 0 || altura >= 3.00) {
+    console.log("Altura inválida!");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida";
+}
+
+if (pesoEhValido && alturaEhValida) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
+
+
+Agora vamos ver se funfa mesmo !
+
