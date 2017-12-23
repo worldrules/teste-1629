06373899e9d23a0a7949e6b133ff1f760745b981
LEONARDO NUNES CARVALHO <worldrulesdev@gmail.com>
2017-12-23 18:12:06 +0000

    New Commit web

--- principal.js
+++ principal.js
@@ -14,12 +14,6 @@ var tdImc = paciente.querySelector(".info-imc");
 var pesoEhValido = true;
 var alturaEhValida = true;
 
-if (peso <= 0 || peso >= 1000) {
-    console.log("Peso inválido!");
-    pesoEhValido = false;
-    tdImc.textContent = "Peso inválido";
-}
-
 if (altura <= 0 || altura >= 3.00) {
     console.log("Altura inválida!");
     alturaEhValida = false;
